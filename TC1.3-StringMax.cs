f14e13b4b68cc11dff72b0e560eeacd824771bfa
DhruvGour <dhruv250800@gmail.com>
2023-06-05 00:52:08 +0530

    [DhruvGour]-adding TC1

---
+++ TC1.3-StringMax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricProblems
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    namespace GenericProblem
+    {
+        internal class TestClass33
+        {
+            public string StringCompare(string first, string second, string third)
+            {
+                if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+                {
+                    return first;
+                }
+                if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+                {
+                    return second;
+                }
+                if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+                {
+                    return third;
+                }
+                return "0";
+            }
+        }
+    }
+
+}
